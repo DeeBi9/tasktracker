9be30e9be70a9fab556b4ae5593c2927ab9d8e4a
Deepanshu Bisht <deepanshudb1@gmail.com>
2025-04-23 02:03:45 +0530

    modify cmd/commands.go
    Signed-off-by: Deepanshu Bisht <deepanshudb1@gmail.com>

--- cmd/commands.go
+++ cmd/commands.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/DeeBi9/tasktracker/tasks"
 	"github.com/spf13/cobra"
@@ -62,8 +63,72 @@ var deleteCmd = &cobra.Command{
 	},
 }
 
+var taskCmd_ = &cobra.Command{
+	Use:   "changestatus",
+	Short: "Change the status of task to to-do",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		arg1, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("Error: Second argument must be an integer")
+			return
+		}
+		err = tasks.Change_status(args[0], arg1)
+		if err != nil {
+			fmt.Printf("Error changing status: %v\n", err)
+			return
+		}
+		fmt.Println("Status changed successfully.")
+	},
+}
+
+var listAll = &cobra.Command{
+	Use:   "listall",
+	Short: "List all the tasks",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		var data tasks.Data
+		data, err = tasks.ListAll()
+		if err != nil {
+			fmt.Printf("Error Listing all tasks")
+			return
+		}
+		fmt.Println("Tasks are :")
+		for _, task := range data.Data {
+			fmt.Printf("ID: %d\nDescription: %s\nStatus: %s\nCreated: %s\nUpdated: %s\n\n",
+				task.Id, task.Description, task.Status, task.CreatedAt.Format(time.RFC822), task.UpdatedAt.Format(time.RFC822))
+			fmt.Printf("\n")
+		}
+	},
+}
+
+var listStatusWise = &cobra.Command{
+	Use:   "list",
+	Short: "List all the tasks",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		var data tasks.Data
+		data, err = tasks.Liststatuswise(args[0])
+		if err != nil {
+			fmt.Printf("Error Listing all tasks")
+			return
+		}
+		fmt.Printf("Tasks of %v\n :", args[0])
+		for _, task := range data.Data {
+			fmt.Printf("ID: %d\nDescription: %s\nStatus: %s\nCreated: %s\nUpdated: %s\n\n",
+				task.Id, task.Description, task.Status, task.CreatedAt.Format(time.RFC822), task.UpdatedAt.Format(time.RFC822))
+			fmt.Printf("\n")
+		}
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(addCmd)
 	rootCmd.AddCommand(updateCmd)
 	rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(taskCmd_)
+	rootCmd.AddCommand(listAll)
+	rootCmd.AddCommand(listStatusWise)
 }
