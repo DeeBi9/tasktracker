e4d77c679052da8d4ac3e216db9aea718556e921
Deepanshu Bisht <deepanshudb1@gmail.com>
2025-04-23 00:24:23 +0530

    modify tasks/task.go
    Signed-off-by: Deepanshu Bisht <deepanshudb1@gmail.com>

--- tasks/task.go
+++ tasks/task.go
@@ -10,118 +10,137 @@ import (
 
 type Task struct {
 	Id          int       `json:"id"`
-	Description string    `json:"string"`
+	Description string    `json:"description"`
 	Status      string    `json:"status"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
 }
+
 type Data struct {
 	Data []Task `json:"task"`
 }
 
-func openJSON() os.File {
-	jsonFile, err := os.Open("tasks.json")
+func openJSON() (*os.File, error) {
+	jsonFile, err := os.OpenFile("tasks.json", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-
-	return *jsonFile
+	return jsonFile, nil
 }
 
 func AddJSON(arg1 string, arg2 string) error {
-	jsonFile := openJSON()
+	jsonFile, err := openJSON()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(&jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var data Data
-	json.Unmarshal(byteValue, &data)
-	lastid := data.Data[len(data.Data)-1].Id
+	if len(byteValue) > 0 {
+		json.Unmarshal(byteValue, &data)
+	} else {
+		data = Data{Data: []Task{}}
+	}
 
-	defer jsonFile.Close()
+	lastid := 0
+	if len(data.Data) > 0 {
+		lastid = data.Data[len(data.Data)-1].Id
+	}
 
 	if arg1 == "add" {
 		updatedTime := time.Now()
-		if len(data.Data) == 0 {
-			lastid = 1
-		}
 
-		d := Data{
-			Data: []Task{
-				{
-					Id:          lastid + 1,
-					Description: arg2,
-					Status:      "To-Do",
-					CreatedAt:   time.Now(),
-					UpdatedAt:   updatedTime,
-				},
-			},
+		newTask := Task{
+			Id:          lastid + 1,
+			Description: arg2,
+			Status:      "To-Do",
+			CreatedAt:   time.Now(),
+			UpdatedAt:   updatedTime,
 		}
-		jsonData, err := json.Marshal(d)
+		data.Data = append(data.Data, newTask)
+
+		jsonData, err := json.MarshalIndent(data, "", "  ")
 		if err != nil {
 			fmt.Println("Error encoding JSON:", err)
 			return err
 		}
-		_, err = jsonFile.Write(jsonData)
+
+		err = os.WriteFile("tasks.json", jsonData, 0644)
 		if err != nil {
 			fmt.Println("Error writing to file:", err)
 			return err
 		}
 		return nil
-	} else {
-		return fmt.Errorf("incorrect argument")
 	}
-
+	return fmt.Errorf("incorrect argument")
 }
 
 func UpdateJSON(arg1 string, arg2 int, arg3 string) error {
-	jsonFile := openJSON()
+	jsonFile, err := openJSON()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(&jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var data Data
-	json.Unmarshal(byteValue, &data)
-
-	defer jsonFile.Close()
+	if len(byteValue) > 0 {
+		json.Unmarshal(byteValue, &data)
+	} else {
+		return fmt.Errorf("no data found")
+	}
 
 	if arg1 == "update" {
 		updatedTime := time.Now()
-		d := Data{
-			Data: []Task{
-				{
-					Description: arg3,
-					UpdatedAt:   updatedTime,
-				},
-			},
-		}
+		found := false
 		for i := range data.Data {
 			if data.Data[i].Id == arg2 {
-				jsonData, err := json.Marshal(d)
-				if err != nil {
-					fmt.Println("Error encoding JSON:", err)
-					return err
-				}
-				_, err = jsonFile.Write(jsonData)
-				if err != nil {
-					fmt.Println("Error writing to file:", err)
-					return err
-				}
+				data.Data[i].Description = arg3
+				data.Data[i].UpdatedAt = updatedTime
+				found = true
+				break
 			}
 		}
+		if !found {
+			return fmt.Errorf("task with id %d not found", arg2)
+		}
+
+		jsonData, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			fmt.Println("Error encoding JSON:", err)
+			return err
+		}
+		err = os.WriteFile("tasks.json", jsonData, 0644)
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+			return err
+		}
 		return nil
-	} else {
-		return fmt.Errorf("incorrect argument")
 	}
+	return fmt.Errorf("incorrect argument")
 }
 
 func DeleteJSON(arg1 string, arg2 int) error {
-	jsonFile := openJSON()
+	jsonFile, err := openJSON()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(&jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var data Data
-	json.Unmarshal(byteValue, &data)
-
-	defer jsonFile.Close()
+	if len(byteValue) > 0 {
+		json.Unmarshal(byteValue, &data)
+	} else {
+		return fmt.Errorf("no data found")
+	}
 
 	if arg1 == "delete" {
 		indextodelete := -1
@@ -131,21 +150,22 @@ func DeleteJSON(arg1 string, arg2 int) error {
 				break
 			}
 		}
-		if indextodelete != -1 {
-			data.Data = append(data.Data[:indextodelete], data.Data[indextodelete+1:]...)
-			jsonData, err := json.Marshal(data)
-			if err != nil {
-				fmt.Println("Error encoding JSON:", err)
-				return err
-			}
-			_, err = jsonFile.Write(jsonData)
-			if err != nil {
-				fmt.Println("Error writing to file:", err)
-				return err
-			}
+		if indextodelete == -1 {
+			return fmt.Errorf("task with id %d not found", arg2)
+		}
+
+		data.Data = append(data.Data[:indextodelete], data.Data[indextodelete+1:]...)
+		jsonData, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			fmt.Println("Error encoding JSON:", err)
+			return err
+		}
+		err = os.WriteFile("tasks.json", jsonData, 0644)
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+			return err
 		}
 		return nil
-	} else {
-		return fmt.Errorf("incorrect argument")
 	}
+	return fmt.Errorf("incorrect argument")
 }
